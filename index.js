c0e43062a91d16c35dbe95a83e196c07ac848b25
ranga-sangala <rangasangala7@gmail.com>
2023-09-26 12:31:02 +0530

    basic working model

---
+++ index.js
@@ -0,0 +1,24 @@
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+const closeBtn = document.querySelector(".close-btn");
+const chatbox = document.querySelector(".chatbox");
+const chatInput = document.querySelector(".chat-input");
+const sendChatBtn = document.querySelector(".chat-input span");
+
+
+const handleChat = () => {
+    // Append the user's message to the chatbox
+    chatbox.appendChild();  
+}
+
+chatInput.addEventListener("keydown", (e) => {
+    // If Enter key is pressed without Shift key and the window 
+    // width is greater than 800px, handle the chat
+    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+        e.preventDefault();
+        handleChat();
+    }
+});
+
+sendChatBtn.addEventListener("click", handleChat);
+closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
+chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
